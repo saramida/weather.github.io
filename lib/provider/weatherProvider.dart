f1a29bb87cc4689ff3796ef84c2684682f8fa7a7
saramida <smcity5@daum.net>
2023-05-01 01:24:33 +0900

    Add files via upload

--- lib/provider/weatherProvider.dart
+++ lib/provider/weatherProvider.dart
@@ -1,1 +1,145 @@
+import 'dart:convert';
+import 'dart:developer';
 
+import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
+import 'package:latlong2/latlong.dart';
+import 'package:location/location.dart';
+
+import '../models/dailyWeather.dart';
+import '../models/weather.dart';
+import '../trace.dart';
+
+class WeatherProvider with ChangeNotifier {
+  String apiKey = '7a7d1d8e4f6f0a47cb70183202fa11c5';
+  LatLng? currentLocation;
+  late Weather weather;
+  DailyWeather currentWeather = DailyWeather();
+  List<DailyWeather> hourlyWeather = [];
+  List<DailyWeather> hourly24Weather = [];
+  List<DailyWeather> fiveDayWeather = [];
+  List<DailyWeather> sevenDayWeather = [];
+  bool isLoading = false;
+  bool isRequestError = false;
+  bool isLocationError = false;
+
+  Future<void> getWeatherData({bool isRefresh = false}) async {
+    isLoading = true;
+    isRequestError = false;
+    isLocationError = false;
+    if (isRefresh) notifyListeners();
+    await Location().requestService().then(
+      (value) async {
+        if (value) {
+          final locData = await Location().getLocation();
+          currentLocation = LatLng(locData.latitude!, locData.longitude!);
+          await getCurrentWeather(currentLocation!);
+          await getDailyWeather(currentLocation!);
+        } else {
+          isLoading = false;
+          isLocationError = true;
+          notifyListeners();
+        }
+      },
+    );
+  }
+
+  Future<void> getCurrentWeather(LatLng location) async {
+    Uri url = Uri.parse(
+      'https://api.openweathermap.org/data/2.5/weather?lat=${location.latitude}&lon=${location.longitude}&units=metric&appid=$apiKey',
+    );
+
+    try {
+      final response = await http.get(url);
+      final extractedData = json.decode(response.body) as Map<String, dynamic>;
+      Trace.DebugPrintln("getCurrentWeather = " + extractedData.toString());
+      weather = Weather.fromJson(extractedData);
+    } catch (error) {
+      print(error);
+      this.isRequestError = true;
+      throw error;
+    } finally {
+      isLoading = false;
+      notifyListeners();
+    }
+  }
+
+  Future<void> getDailyWeather(LatLng location) async {
+    isLoading = true;
+    notifyListeners();
+
+    Uri dailyUrl = Uri.parse(
+      'https://api.openweathermap.org/data/3.0/onecall?lat=${location.latitude}&lon=${location.longitude}&units=metric&exclude=minutely,current&appid=$apiKey',
+    );
+
+    try {
+      final response = await http.get(dailyUrl);
+      inspect(response.body);
+      final dailyData = json.decode(response.body) as Map<String, dynamic>;
+      Trace.DebugPrintln("getDailyWeather = " + dailyData.toString());
+      currentWeather = DailyWeather.fromJson(dailyData);
+
+      List<DailyWeather> tempHourly = [];
+      List<DailyWeather> temp24Hour = [];
+      List<DailyWeather> tempSevenDay = [];
+      List items = dailyData['daily'];
+      List itemsHourly = dailyData['hourly'];
+      tempHourly = itemsHourly
+          .map((item) => DailyWeather.fromHourlyJson(item))
+          .toList()
+          .skip(1)
+          .take(3)
+          .toList();
+      temp24Hour = itemsHourly
+          .map((item) => DailyWeather.fromHourlyJson(item))
+          .toList()
+          .skip(1)
+          .take(24)
+          .toList();
+      tempSevenDay = items
+          .map((item) => DailyWeather.fromDailyJson(item))
+          .toList()
+          .skip(1)
+          .take(7)
+          .toList();
+      hourlyWeather = tempHourly;
+      hourly24Weather = temp24Hour;
+      sevenDayWeather = tempSevenDay;
+    } catch (error) {
+      print(error);
+      this.isRequestError = true;
+      throw error;
+    } finally {
+      isLoading = false;
+      notifyListeners();
+    }
+  }
+
+  Future<void> searchWeatherWithLocation(String location) async {
+    Uri url = Uri.parse(
+      'https://api.openweathermap.org/data/2.5/weather?q=$location&units=metric&appid=$apiKey',
+    );
+    try {
+      final response = await http.get(url);
+      final extractedData = json.decode(response.body) as Map<String, dynamic>;
+      weather = Weather.fromJson(extractedData);
+      Trace.DebugPrintln("searchWeatherWithLocation = " + extractedData.toString());
+      getDailyWeather(new LatLng(weather.lat,weather.long));
+      getCurrentWeather(new LatLng(weather.lat,weather.long));
+    } catch (error) {
+      this.getWeatherData();
+      //this.isRequestError = true;
+      //throw error;
+    }
+  }
+
+  Future<void> searchWeather({required String location}) async {
+    isLoading = true;
+    isRequestError = false;
+    isLocationError = false;
+    double latitude = weather.lat;
+    double longitude = weather.long;
+    await searchWeatherWithLocation(location);
+    //await getDailyWeather(LatLng(latitude, longitude));
+  }
+}
